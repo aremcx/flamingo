6a35468a73670eeed7fcd7930b641f3511f4e4c1
Răzvan-Gabriel Geangu <razvan.geangu@gmail.com>
2020-10-10 16:28:54 +0100

    feat(#5): add draw-hand tf example

--- packages/flamingo-ui/src/app/containers/HomePage/index.tsx
+++ packages/flamingo-ui/src/app/containers/HomePage/index.tsx
@@ -1,20 +1,72 @@
+import * as handpose from '@tensorflow-models/handpose';
+import '@tensorflow/tfjs';
 import { translations } from 'locales/i18n';
-import React from 'react';
+import React, { useRef } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
-import styled from 'styled-components/macro';
+import ReactWebcam from 'react-webcam';
+import styled, { css } from 'styled-components/macro';
+import { drawHand } from 'utils/canvas/draw-hand';
 
-const AppName = styled.span`
-  align-content: center;
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  height: 100vh;
-  justify-content: center;
+const shared = css`
+  height: 480px;
+  left: 0;
+  margin-left: auto;
+  margin-right: auto;
+  position: absolute;
+  right: 0;
+  text-align: center;
+  width: 640px;
+  z-index: 9;
+`;
+
+const Camera = styled(ReactWebcam)`
+  ${shared}
+`;
+
+const Canvas = styled.canvas`
+  ${shared}
 `;
 
 export function HomePage() {
   const { t } = useTranslation();
+
+  const cameraRef = useRef<ReactWebcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const runHandpose = async () => {
+    const net = await handpose.load();
+
+    /* Loop and detect hands */
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net: any) => {
+    /* Check data is available */
+    if (cameraRef.current != null && cameraRef.current!.video!.readyState === 4) {
+      /* Get video properties */
+      const { video } = cameraRef.current;
+      const { videoWidth, videoHeight } = video!;
+
+      /* Set video height and width */
+      video!.width = videoWidth;
+      video!.height = videoHeight;
+
+      /* Set canvas height and width */
+      canvasRef.current!.width = videoWidth;
+      canvasRef.current!.height = videoHeight;
+      /* Make detections */
+      const hand = await net.estimateHands(video);
+
+      /* Draw mesh */
+      const context = canvasRef.current!.getContext('2d')!;
+      drawHand(hand, context);
+    }
+  };
+
+  runHandpose();
 
   return (
     <>
@@ -22,7 +74,8 @@ export function HomePage() {
         <title>Home Page</title>
         <meta name="description" content={t(translations.app.quote)} />
       </Helmet>
-      <AppName>{t(translations.app.name)}</AppName>
+      <Camera ref={cameraRef as any} />
+      <Canvas ref={canvasRef} />
     </>
   );
 }
---
+++ packages/flamingo-ui/src/utils/canvas/draw-hand.ts
@@ -0,0 +1,69 @@
+const fingerJoints = {
+  thumb: [0, 1, 2, 3, 4],
+  index: [0, 5, 6, 7, 8],
+  middle: [0, 9, 10, 11, 12],
+  ring: [0, 13, 14, 15, 16],
+  pinky: [0, 17, 18, 19, 20],
+};
+
+// Infinity Gauntlet Style
+const style = [
+  { color: 'yellow', size: 15 },
+  { color: 'gold', size: 6 },
+  { color: 'green', size: 10 },
+  { color: 'gold', size: 6 },
+  { color: 'gold', size: 6 },
+  { color: 'purple', size: 10 },
+  { color: 'gold', size: 6 },
+  { color: 'gold', size: 6 },
+  { color: 'gold', size: 6 },
+  { color: 'blue', size: 10 },
+  { color: 'gold', size: 6 },
+  { color: 'gold', size: 6 },
+  { color: 'gold', size: 6 },
+  { color: 'red', size: 10 },
+  { color: 'gold', size: 6 },
+  { color: 'gold', size: 6 },
+  { color: 'gold', size: 6 },
+  { color: 'orange', size: 10 },
+  { color: 'gold', size: 6 },
+  { color: 'gold', size: 6 },
+  { color: 'gold', size: 6 },
+];
+
+export const drawHand = (
+  predictions: Array<{ landmarks: Array<Array<number>> }>,
+  context: CanvasRenderingContext2D,
+) => {
+  predictions.forEach(({ landmarks }) => {
+    Object.keys(fingerJoints).forEach(finger => {
+      fingerJoints[finger as keyof typeof fingerJoints].forEach((joint, jointIndex) => {
+        if (jointIndex === fingerJoints[finger].length - 1) return;
+
+        /* Get pairs of joints */
+        const firstJointIndex = joint;
+        const secondJointIndex = fingerJoints[finger][jointIndex + 1];
+
+        /* Draw path */
+        context.beginPath();
+        context.moveTo(landmarks[firstJointIndex][0], landmarks[firstJointIndex][1]);
+        context.lineTo(landmarks[secondJointIndex][0], landmarks[secondJointIndex][1]);
+        context.strokeStyle = 'plum';
+        context.lineWidth = 4;
+        context.stroke();
+      });
+    });
+
+    landmarks.forEach((landmark, landmarkIndex) => {
+      const { 0: x, 1: y } = landmark;
+
+      /* Start drawing */
+      context.beginPath();
+      context.arc(x, y, style[landmarkIndex].size, 0, 3 * Math.PI);
+
+      /* Set line color */
+      context.fillStyle = style[landmarkIndex].color;
+      context.fill();
+    });
+  });
+};
